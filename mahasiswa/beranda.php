198eabf7e6213e5f15facf2ad80a8c341243a820
ersadishla <ersadishlahuddin@gmail.com>
2021-03-31 13:33:12 +0700

    beranda perusahaan dan mahasiswa

---
+++ mahasiswa/beranda.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php include "../partials/head.php"?>
+    <link rel="stylesheet" href="../assets/css/dashforge.profile.css">
+  </head>
+  <body class="page-profile">
+
+    <?php include "../partials/menu.php"?>
+
+    <div class="content ht-100v pd-0">
+
+      <?php include "../partials/navbar.php"?>
+
+      <div class="content-body">
+        <div class="container pd-x-0 tx-13">
+          <div class="media d-block d-lg-flex">
+            <div class="media-body mg-t-40 mg-lg-t-0 pd-lg-x-10">
+
+              <div class="profile-update-option bg-white ht-50 bd d-flex justify-content-end mg-b-20 mg-lg-b-25 rounded">
+                <div class="d-flex align-items-center pd-x-20 mg-r-auto">
+                  <i data-feather="edit-3"></i> <a href="#modalEditContact" data-toggle="modal" class="link-03 mg-l-10"><span class="d-none d-sm-inline">Apa yang ingin</span> anda bagikan?</a>
+                </div>
+                <div class="wd-50 bd-l d-flex align-items-center justify-content-center">
+                  <a href="" class="link-03" data-toggle="tooltip" title="Unggah Foto"><i data-feather="image"></i></a>
+                </div>
+                <div class="wd-50 bd-l d-flex align-items-center justify-content-center">
+                  <a href="" class="link-03" data-toggle="tooltip" title="Bagikan Tautan"><i data-feather="link"></i></a>
+                </div>
+                <div class="wd-50 bd-l d-flex align-items-center justify-content-center">
+                  <a href="" class="link-03" data-toggle="tooltip" title="Unggah Dokumen"><i data-feather="file-text"></i></a>
+                </div>
+              </div>
+
+              <div class="card mg-b-20 mg-lg-b-25">
+                <div class="card-header pd-y-15 pd-x-20 d-flex align-items-center justify-content-between">
+                  <h6 class="tx-13 tx-spacing-1 tx-uppercase tx-semibold mg-b-0">Latest Activity</h6>
+                  <nav class="nav nav-icon-only">
+                    <a href="" class="nav-link"><i data-feather="more-horizontal"></i></a>
+                  </nav>
+                </div><!-- card-header -->
+                <div class="card-body pd-20 pd-lg-25">
+                  <div class="media align-items-center mg-b-20">
+                    <div class="avatar avatar-online"><img src="https://via.placeholder.com/500" class="rounded-circle" alt=""></div>
+                    <div class="media-body pd-l-15">
+                      <h6 class="mg-b-3">Nora Zoey</h6>
+                      <span class="d-block tx-13 tx-color-03">Founder of PT. XYZ</span>
+                    </div>
+                    <span class="d-none d-sm-block tx-12 tx-color-03 align-self-start">5 hours ago</span>
+                  </div><!-- media -->
+                  <p class="mg-b-20">Our team is expanding again. We are looking for a Product Manager and Software Engineer to drive our new aspects of our capital projects. If you're interested, please drop a comment here or simply message me. <a href="">#softwareengineer</a> <a href="">#engineering</a></p>
+
+                  <div class="bd bg-gray-50 pd-y-15 pd-x-15 pd-sm-x-20">
+                    <h6 class="tx-15 mg-b-3">We're hiring of Product Manager</h6>
+                    <p class="mg-b-0 tx-14">Full-time, $60,000 - $80,000 annual</p>
+                    <span class="tx-13 tx-color-03">Bay Area, San Francisco, CA</span>
+                  </div>
+                </div>
+                <div class="card-footer bg-transparent pd-y-10 pd-sm-y-15 pd-x-10 pd-sm-x-20">
+                  <nav class="nav nav-with-icon tx-13">
+                    <a href="" class="nav-link"><i data-feather="thumbs-up"></i> Like</a>
+                    <a href="" class="nav-link"><i data-feather="message-square"></i> Comment</a>
+                    <a href="" class="nav-link"><i data-feather="share"></i> Share</a>
+                  </nav>
+                </div><!-- card-footer -->
+              </div><!-- card -->
+
+              <div class="card mg-b-20 mg-lg-b-25">
+                <div class="card-body pd-20 pd-lg-25">
+                  <div class="media align-items-center mg-b-20">
+                    <div class="avatar avatar-online"><img src="https://via.placeholder.com/500" class="rounded-circle" alt=""></div>
+                    <div class="media-body pd-l-15">
+                      <h6 class="mg-b-3">Liam Payne</h6>
+                      <span class="d-block tx-13 tx-color-03">Business Expert</span>
+                    </div>
+                    <span class="d-none d-sm-block tx-12 tx-color-03 align-self-start">5 hours ago</span>
+                  </div><!-- media -->
+                  <p class="">Our team is expanding again. We are looking for a Product Manager and Software Engineer to drive our new aspects of our capital projects. If you're interested, please drop a comment here or simply message me. <a href="">#softwareengineer</a> <a href="">#engineering</a></p>
+                </div>
+                <div class="card-footer bg-transparent pd-y-10 pd-sm-y-15 pd-x-10 pd-sm-x-20">
+                  <nav class="nav nav-with-icon tx-13">
+                    <a href="" class="nav-link"><i data-feather="thumbs-up"></i> Like</a>
+                    <a href="" class="nav-link"><i data-feather="message-square"></i> Comment</a>
+                    <a href="" class="nav-link"><i data-feather="share"></i> Share</a>
+                  </nav>
+                </div><!-- card-footer -->
+              </div><!-- card -->
+
+
+            </div><!-- media-body -->
+
+
+            <div class="profile-sidebar profile-sidebar-two mg-t-40 mg-lg-t-0 pd-lg-l-15">
+              <div class="row">
+                <div class="col-sm-6 col-md-5 col-lg">
+                  <div class="d-flex align-items-center justify-content-between mg-b-20">
+                    <h6 class="tx-13 tx-uppercase tx-semibold mg-b-0">lowongan untuk anda</h6>
+                  </div>
+                  <ul class="list-unstyled media-list mg-b-15">
+                    <li class="media align-items-center">
+                      <a href=""><div class="avatar"><img src="https://via.placeholder.com/500" class="rounded-circle" alt=""></div></a>
+                      <div class="media-body pd-l-15">
+                        <p class="tx-medium mg-b-2"><a href="" class="link-01">UI/UX Designer</a></p>
+                        <span class="tx-12 tx-color-03">PT. Aris Wijaya</span>
+                      </div>
+                    </li>
+                    <li class="media align-items-center">
+                      <a href=""><div class="avatar"><img src="https://via.placeholder.com/500" class="rounded-circle" alt=""></div></a>
+                      <div class="media-body pd-l-15">
+                        <p class="tx-medium mg-b-2"><a href="" class="link-01">Senior Business Analyst</a></p>
+                        <span class="tx-12 tx-color-03">Tokopedia Indonesia</span>
+                      </div>
+                    </li>
+                    <li class="media align-items-center">
+                      <a href=""><div class="avatar"><img src="https://via.placeholder.com/500" class="rounded-circle" alt=""></div></a>
+                      <div class="media-body pd-l-15">
+                        <p class="tx-medium mg-b-2"><a href="" class="link-01">Product Designer</a></p>
+                        <span class="tx-12 tx-color-03">Blibli.com</span>
+                      </div>
+                    </li>
+                    <li class="media align-items-center">
+                      <a href=""><div class="avatar"><img src="https://via.placeholder.com/500" class="rounded-circle" alt=""></div></a>
+                      <div class="media-body pd-l-15">
+                        <p class="tx-medium mg-b-2"><a href="" class="link-01">UX Writer</a></p>
+                        <span class="tx-12 tx-color-03">Tiket.com</span>
+                      </div>
+                    </li>
+                    <li class="media align-items-center">
+                      <a href=""><div class="avatar"><img src="https://via.placeholder.com/500" class="rounded-circle" alt=""></div></a>
+                      <div class="media-body pd-l-15">
+                        <p class="tx-medium mg-b-2"><a href="" class="link-01">Front-end Engineer</a></p>
+                        <span class="d-block tx-12 tx-color-03">Pertamina</span>
+                      </div>
+                    </li>
+                  </ul>
+                </div><!-- col --> 
+                <div class="col-sm-6 col-md-5 col-lg mg-t-40">
+                  <div class="d-flex align-items-center justify-content-between mg-b-20">
+                    <h6 class="tx-13 tx-spacing-1 tx-uppercase tx-semibold mg-b-0">yang Mungkin Anda Kenal</h6>
+                  </div>
+                  <ul class="list-unstyled media-list mg-b-15">
+                    <li class="media align-items-center">
+                      <a href=""><div class="avatar avatar-online"><img src="https://via.placeholder.com/350" class="rounded-circle" alt=""></div></a>
+                      <div class="media-body pd-l-15">
+                        <p class="tx-medium mg-b-2"><a href="" class="link-01">Roy Recamadas</a></p>
+                        <span class="tx-12 tx-color-03">UI/UX Designer &amp; Developer</span>
+                      </div>
+                    </li>
+                    <li class="media align-items-center">
+                      <a href=""><div class="avatar avatar-offline"><img src="https://via.placeholder.com/600" class="rounded-circle" alt=""></div></a>
+                      <div class="media-body pd-l-15">
+                        <p class="tx-medium mg-b-2"><a href="" class="link-01">Raymart Serencio</a></p>
+                        <span class="tx-12 tx-color-03">Full-Stack Developer</span>
+                      </div>
+                    </li>
+                    <li class="media align-items-center">
+                      <a href=""><div class="avatar avatar-offline"><span class="avatar-initial rounded-circle bg-teal">R</span></div></a>
+                      <div class="media-body pd-l-15">
+                        <p class="tx-medium mg-b-2"><a href="" class="link-01">Rolando Paloso Jr</a></p>
+                        <span class="tx-12 tx-color-03">Licensed Architect</span>
+                      </div>
+                    </li>
+                    <li class="media align-items-center">
+                      <a href=""><div class="avatar avatar-online"><span class="avatar-initial rounded-circle bg-indigo">R</span></div></a>
+                      <div class="media-body pd-l-15">
+                        <p class="tx-medium mg-b-2"><a href="" class="link-01">Robert Restificar</a></p>
+                        <span class="tx-12 tx-color-03">Business Analyst</span>
+                      </div>
+                    </li>
+                    <li class="media align-items-center">
+                      <a href=""><div class="avatar avatar-online"><span class="avatar-initial rounded-circle bg-gray-600">A</span></div></a>
+                      <div class="media-body pd-l-15">
+                        <p class="tx-medium mg-b-2"><a href="" class="link-01">Archie Cantones</a></p>
+                        <span class="tx-12 tx-color-03">Senior Software Architect</span>
+                      </div>
+                    </li>
+                  </ul>
+                </div><!-- col -->
+              </div><!-- row -->
+            </div><!-- profile-sidebar -->
+          </div><!-- media -->
+        </div><!-- container -->
+      </div><!-- content-body -->
+    </div><!-- content -->
+
+    <div class="modal fade effect-scale" id="modalEditContact" tabindex="-1" role="dialog" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-body pd-20 pd-sm-30">
+            <button type="button" class="close pos-absolute t-15 r-20" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+
+            <h5 class="tx-18 tx-sm-20 mg-b-20">Buat Postingan</h5>
+
+            <div class="d-sm-flex">
+              <div class="flex-fill">
+                <textarea class="form-control" rows="2" placeholder="Apa yang ingin anda bagikan?"></textarea>
+                <button type="button" class="btn btn-xs btn-light rounded-pill mg-t-10"><i data-feather="image"></i><span class="d-none d-sm-inline mg-l-5">Gambar</span></button>
+                <button type="button" class="btn btn-xs btn-light rounded-pill mg-t-10"><i data-feather="link"></i><span class="d-none d-sm-inline mg-l-5">Tautan</span></button>
+                <button type="button" class="btn btn-xs btn-light rounded-pill mg-t-10"><i data-feather="file-text"></i><span class="d-none d-sm-inline mg-l-5">Dokumen</span></button>
+              </div><!-- col -->
+            </div>
+          </div>
+          <div class="modal-footer">
+            <div class="wd-100p d-flex flex-column flex-sm-row justify-content-end">
+              <div class="dropup mg-b-15 mg-sm-b-0 mg-sm-r-auto">
+                <div class="dropdown-menu tx-13">
+                  <a href="" class="dropdown-item">Email</a>
+                  <a href="" class="dropdown-item">Phone</a>
+                  <a href="" class="dropdown-item">Address</a>
+                  <a href="" class="dropdown-item">Custom</a>
+                </div><!-- dropdown-menu -->
+              </div>
+              <button type="button" class="btn btn-primary mg-b-5 mg-sm-b-0">Posting Sekarang</button>
+            </div>
+          </div><!-- modal-footer -->
+        </div><!-- modal-content -->
+      </div><!-- modal-dialog -->
+    </div><!-- modal -->
+
+    <?php include "../partials/script.php"?>
+  </body>
+</html>
---
+++ perusahaan/beranda.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php include "../partials/head.php"?>
+    <link rel="stylesheet" href="../assets/css/dashforge.profile.css">
+  </head>
+  <body class="page-profile">
+
+    <?php include "../partials/menu.php"?>
+
+    <div class="content ht-100v pd-0">
+
+      <?php include "../partials/navbar.php"?>
+
+      <div class="content-body">
+        <div class="container pd-x-0 tx-13">
+          <div class="media d-block d-lg-flex">
+            <div class="media-body mg-t-40 mg-lg-t-0 pd-lg-x-10">
+
+              <div class="profile-update-option bg-white ht-50 bd d-flex justify-content-end mg-b-20 mg-lg-b-25 rounded">
+                <div class="d-flex align-items-center pd-x-20 mg-r-auto">
+                  <i data-feather="edit-3"></i> <a href="#modalEditContact" data-toggle="modal" class="link-03 mg-l-10"><span class="d-none d-sm-inline">Apa yang ingin</span> anda bagikan?</a>
+                </div>
+                <div class="wd-50 bd-l d-flex align-items-center justify-content-center">
+                  <a href="" class="link-03" data-toggle="tooltip" title="Unggah Foto"><i data-feather="image"></i></a>
+                </div>
+                <div class="wd-50 bd-l d-flex align-items-center justify-content-center">
+                  <a href="" class="link-03" data-toggle="tooltip" title="Bagikan Tautan"><i data-feather="link"></i></a>
+                </div>
+                <div class="wd-50 bd-l d-flex align-items-center justify-content-center">
+                  <a href="" class="link-03" data-toggle="tooltip" title="Unggah Dokumen"><i data-feather="file-text"></i></a>
+                </div>
+              </div>
+
+              <div class="card mg-b-20 mg-lg-b-25">
+                <div class="card-header pd-y-15 pd-x-20 d-flex align-items-center justify-content-between">
+                  <h6 class="tx-13 tx-spacing-1 tx-uppercase tx-semibold mg-b-0">Latest Activity</h6>
+                  <nav class="nav nav-icon-only">
+                    <a href="" class="nav-link"><i data-feather="more-horizontal"></i></a>
+                  </nav>
+                </div><!-- card-header -->
+                <div class="card-body pd-20 pd-lg-25">
+                  <div class="media align-items-center mg-b-20">
+                    <div class="avatar avatar-online"><img src="https://via.placeholder.com/500" class="rounded-circle" alt=""></div>
+                    <div class="media-body pd-l-15">
+                      <h6 class="mg-b-3">Nora Zoey</h6>
+                      <span class="d-block tx-13 tx-color-03">Founder of PT. XYZ</span>
+                    </div>
+                    <span class="d-none d-sm-block tx-12 tx-color-03 align-self-start">5 hours ago</span>
+                  </div><!-- media -->
+                  <p class="mg-b-20">Our team is expanding again. We are looking for a Product Manager and Software Engineer to drive our new aspects of our capital projects. If you're interested, please drop a comment here or simply message me. <a href="">#softwareengineer</a> <a href="">#engineering</a></p>
+
+                  <div class="bd bg-gray-50 pd-y-15 pd-x-15 pd-sm-x-20">
+                    <h6 class="tx-15 mg-b-3">We're hiring of Product Manager</h6>
+                    <p class="mg-b-0 tx-14">Full-time, $60,000 - $80,000 annual</p>
+                    <span class="tx-13 tx-color-03">Bay Area, San Francisco, CA</span>
+                  </div>
+                </div>
+                <div class="card-footer bg-transparent pd-y-10 pd-sm-y-15 pd-x-10 pd-sm-x-20">
+                  <nav class="nav nav-with-icon tx-13">
+                    <a href="" class="nav-link"><i data-feather="thumbs-up"></i> Like</a>
+                    <a href="" class="nav-link"><i data-feather="message-square"></i> Comment</a>
+                    <a href="" class="nav-link"><i data-feather="share"></i> Share</a>
+                  </nav>
+                </div><!-- card-footer -->
+              </div><!-- card -->
+
+              <div class="card mg-b-20 mg-lg-b-25">
+                <div class="card-body pd-20 pd-lg-25">
+                  <div class="media align-items-center mg-b-20">
+                    <div class="avatar avatar-online"><img src="https://via.placeholder.com/500" class="rounded-circle" alt=""></div>
+                    <div class="media-body pd-l-15">
+                      <h6 class="mg-b-3">Liam Payne</h6>
+                      <span class="d-block tx-13 tx-color-03">Business Expert</span>
+                    </div>
+                    <span class="d-none d-sm-block tx-12 tx-color-03 align-self-start">5 hours ago</span>
+                  </div><!-- media -->
+                  <p class="">Our team is expanding again. We are looking for a Product Manager and Software Engineer to drive our new aspects of our capital projects. If you're interested, please drop a comment here or simply message me. <a href="">#softwareengineer</a> <a href="">#engineering</a></p>
+                </div>
+                <div class="card-footer bg-transparent pd-y-10 pd-sm-y-15 pd-x-10 pd-sm-x-20">
+                  <nav class="nav nav-with-icon tx-13">
+                    <a href="" class="nav-link"><i data-feather="thumbs-up"></i> Like</a>
+                    <a href="" class="nav-link"><i data-feather="message-square"></i> Comment</a>
+                    <a href="" class="nav-link"><i data-feather="share"></i> Share</a>
+                  </nav>
+                </div><!-- card-footer -->
+              </div><!-- card -->
+
+
+            </div><!-- media-body -->
+
+
+            <div class="profile-sidebar profile-sidebar-two mg-t-40 mg-lg-t-0 pd-lg-l-15">
+              <div class="row">
+                <div class="col-sm-6 col-md-5 col-lg">
+                  <div class="d-flex align-items-center justify-content-between mg-b-20">
+                    <h6 class="tx-13 tx-uppercase tx-semibold mg-b-0">calon pegawai untuk anda</h6>
+                  </div>
+                  <ul class="list-unstyled media-list mg-b-15">
+                    <li class="media align-items-center">
+                      <a href=""><div class="avatar"><img src="https://via.placeholder.com/500" class="rounded-circle" alt=""></div></a>
+                      <div class="media-body pd-l-15">
+                        <p class="tx-medium mg-b-2"><a href="" class="link-01">Aldi Wijaya</a></p>
+                        <span class="tx-12 tx-color-03">Fresh Graduate ITS</span>
+                      </div>
+                    </li>
+                    <li class="media align-items-center">
+                      <a href=""><div class="avatar"><img src="https://via.placeholder.com/500" class="rounded-circle" alt=""></div></a>
+                      <div class="media-body pd-l-15">
+                        <p class="tx-medium mg-b-2"><a href="" class="link-01">Jaler Dio</a></p>
+                        <span class="tx-12 tx-color-03">Fresh Graduate ITS</span>
+                      </div>
+                    </li>
+                    <li class="media align-items-center">
+                      <a href=""><div class="avatar"><img src="https://via.placeholder.com/500" class="rounded-circle" alt=""></div></a>
+                      <div class="media-body pd-l-15">
+                        <p class="tx-medium mg-b-2"><a href="" class="link-01">Bintang Nuralamsyah</a></p>
+                        <span class="tx-12 tx-color-03">Fresh Graduate ITS</span>
+                      </div>
+                    </li>
+                    <li class="media align-items-center">
+                      <a href=""><div class="avatar"><img src="https://via.placeholder.com/500" class="rounded-circle" alt=""></div></a>
+                      <div class="media-body pd-l-15">
+                        <p class="tx-medium mg-b-2"><a href="" class="link-01">Ersad Islahudin</a></p>
+                        <span class="tx-12 tx-color-03">Fresh Graduate ITS</span>
+                      </div>
+                    </li>
+                    <li class="media align-items-center">
+                      <a href=""><div class="avatar"><img src="https://via.placeholder.com/500" class="rounded-circle" alt=""></div></a>
+                      <div class="media-body pd-l-15">
+                        <p class="tx-medium mg-b-2"><a href="" class="link-01">Paramastri</a></p>
+                        <span class="d-block tx-12 tx-color-03">Fresh Graduate ITS</span>
+                      </div>
+                    </li>
+                  </ul>
+                </div><!-- col -->
+              </div><!-- row -->
+            </div><!-- profile-sidebar -->
+          </div><!-- media -->
+        </div><!-- container -->
+      </div><!-- content-body -->
+    </div><!-- content -->
+
+    <div class="modal fade effect-scale" id="modalEditContact" tabindex="-1" role="dialog" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-body pd-20 pd-sm-30">
+            <button type="button" class="close pos-absolute t-15 r-20" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+
+            <h5 class="tx-18 tx-sm-20 mg-b-20">Buat Postingan</h5>
+
+            <div class="d-sm-flex">
+              <div class="flex-fill">
+                <textarea class="form-control" rows="2" placeholder="Apa yang ingin anda bagikan?"></textarea>
+                <button type="button" class="btn btn-xs btn-light rounded-pill mg-t-10"><i data-feather="image"></i><span class="d-none d-sm-inline mg-l-5">Gambar</span></button>
+                <button type="button" class="btn btn-xs btn-light rounded-pill mg-t-10"><i data-feather="link"></i><span class="d-none d-sm-inline mg-l-5">Tautan</span></button>
+                <button type="button" class="btn btn-xs btn-light rounded-pill mg-t-10"><i data-feather="file-text"></i><span class="d-none d-sm-inline mg-l-5">Dokumen</span></button>
+              </div><!-- col -->
+            </div>
+          </div>
+          <div class="modal-footer">
+            <div class="wd-100p d-flex flex-column flex-sm-row justify-content-end">
+              <div class="dropup mg-b-15 mg-sm-b-0 mg-sm-r-auto">
+                <div class="dropdown-menu tx-13">
+                  <a href="" class="dropdown-item">Email</a>
+                  <a href="" class="dropdown-item">Phone</a>
+                  <a href="" class="dropdown-item">Address</a>
+                  <a href="" class="dropdown-item">Custom</a>
+                </div><!-- dropdown-menu -->
+              </div>
+              <button type="button" class="btn btn-primary mg-b-5 mg-sm-b-0">Posting Sekarang</button>
+            </div>
+          </div><!-- modal-footer -->
+        </div><!-- modal-content -->
+      </div><!-- modal-dialog -->
+    </div><!-- modal -->
+
+    <?php include "../partials/script.php"?>
+  </body>
+</html>
