ffd7bfc338f47932b999fbca900148bca289b162
Jaler Dio Fadhilah <37465606+jalerdio@users.noreply.github.com>
2021-04-14 10:22:51 +0700

    update profil mhs dan mhs lain

--- assets/js/dashforge.js
+++ assets/js/dashforge.js
@@ -137,3 +137,37 @@ $("document").ready(function() {
   });
 
 })
+
+$(function(){
+  'use strict'
+
+  // The code below is for demo purposes only.
+  // For you to not be confused, please refer to
+  // Off-Canvas starter template in Collections
+
+  $('.off-canvas-menu').on('click', function(e){
+    e.preventDefault();
+    var target = $(this).attr('href');
+    $(target).addClass('show');
+  });
+
+
+  $('.off-canvas .close').on('click', function(e){
+    e.preventDefault();
+    $(this).closest('.off-canvas').removeClass('show');
+  })
+
+  $(document).on('click touchstart', function(e){
+    e.stopPropagation();
+
+    // closing of sidebar menu when clicking outside of it
+    if(!$(e.target).closest('.off-canvas-menu').length) {
+      var offCanvas = $(e.target).closest('.off-canvas').length;
+      if(!offCanvas) {
+        $('.off-canvas.show').removeClass('show');
+      }
+    }
+  });
+
+
+});
