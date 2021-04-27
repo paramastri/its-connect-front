819589725618449863f1980488958936d3e0ade0
dio <jalerdio@gmail.com>
2021-04-27 12:50:59 +0700

    update profile picture at kelola akun mhs

--- assets/js/dashforge.js
+++ assets/js/dashforge.js
@@ -171,3 +171,18 @@ $(function(){
 
 
 });
+
+$("#profileImage").click(function(e) {
+  $("#imageUpload").click();
+});
+
+function fasterPreview( uploader ) {
+  if ( uploader.files && uploader.files[0] ){
+        $('#profileImage').attr('src', 
+           window.URL.createObjectURL(uploader.files[0]) );
+  }
+}
+
+$("#imageUpload").change(function(){
+  fasterPreview( this );
+});
